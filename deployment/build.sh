f8e7f357f70fc5bac4ab2a4067485ec1e63d9208
Tomusdrw <tomusdrw@gmail.com>
2016-01-05 17:42:23 +0100

    Removing node_modules of html-ts before packing

--- deployment/build.sh
+++ deployment/build.sh
@@ -18,6 +18,7 @@ cd -
 
 echo '----------------------Molding----------------------------------'
 rm -rf runner/node_modules
+rm -rf runner/runners/workers/html-ts/node_modules
 rm -rf static || true
 cp slides/build static -r
 tar cvzf xplarunner.tar.gz runner static
