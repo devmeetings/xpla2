8c972c96961e2f88bf7aa5aa7c1474b05e615905
Tomusdrw <tomusdrw@gmail.com>
2016-05-11 18:28:28 +0200

    Simple worker supporting new files

--- runner/runners/simple-worker.js
+++ runner/runners/simple-worker.js
@@ -4,50 +4,15 @@ module.exports = function createWorker (queueName) {
   return function (data, onUpdate) {
     const onData = (outputData) => {
       const timestamp = Date.now();
+      const onlyOutput = outputData.newFiles || !outputData.success;
 
       const newFiles = [
         {
-          name: 'index.html',
-          content: `
-            <html>
-              <head>
-              </head>
-              <body>
-              ${getResultsAsHtml(outputData)}
-              <script src="/cdn/fetch/0.9.0/fetch.min.js"></script>
-              <script>
-                (function() {
-                  var currentTimestamp = ${timestamp};
-                  var loc = "" + window.location;
-                  var check = loc.replace(/^(.+)\\/.*$/, '$1.json/timestamp');
-                  var noOfChecks = 50;
-
-                  function checkNewVersion() {
-                    fetch(check).then(function (res) {
-                      return res.json();
-                    }).then(function(timestamp) {
-                      if (timestamp.timestamp !== currentTimestamp) {
-                        window.location.reload();
-                      }
-
-                      if (noOfChecks > 0) {
-                        noOfChecks--;
-                        setTimeout(checkNewVersion, 700 + (200 - noOfChecks * 4));
-                      }
-                    }, function () {
-                      setTimeout(checkNewVersion, 2000);
-                    });
-                  }
-
-                  setTimeout(checkNewVersion, 200);
-                }());
-              </script>
-              </body>
-            </html>
-          `,
+          name: onlyOutput ? 'index.html' : '_logs.html',
+          content: getLogsFile(outputData, timestamp),
           mimetype: 'text/html'
         }
-      ];
+      ].concat(outputData.newFiles || []);
 
       return {
         success: outputData.success,
@@ -67,14 +32,57 @@ module.exports = function createWorker (queueName) {
   };
 };
 
+function getLogsFile (data, timestamp) {
+  return `
+    <html>
+      <head>
+      </head>
+      <body>
+      ${getResultsAsHtml(data)}
+      <script src="/cdn/fetch/0.9.0/fetch.min.js"></script>
+      <script>
+        (function() {
+          var currentTimestamp = ${timestamp};
+          var loc = "" + window.location;
+          var check = loc.replace(/^(.+)\\/.*$/, '$1.json/timestamp');
+          var noOfChecks = 50;
+
+          function checkNewVersion() {
+            fetch(check).then(function (res) {
+              return res.json();
+            }).then(function(timestamp) {
+              if (timestamp.timestamp !== currentTimestamp) {
+                window.location.reload();
+              }
+
+              if (noOfChecks > 0) {
+                noOfChecks--;
+                setTimeout(checkNewVersion, 700 + (200 - noOfChecks * 4));
+              }
+            }, function () {
+              setTimeout(checkNewVersion, 2000);
+            });
+          }
+
+          setTimeout(checkNewVersion, 200);
+        }());
+      </script>
+      </body>
+    </html>
+  `;
+}
+
 function getResultsAsHtml (data) {
   if (data.success) {
     return `<pre>${data.result.join('\n')}</pre>`;
   }
+
+  var errors = data.errors || [];
+  var result = data.result || [];
   return `<div style="color: red">
-    <pre>${data.errors.join('\n')}</pre>
+    <pre>${errors.join('\n')}</pre>
     <h3>Output:</h3>
-    <pre>${data.result.join('\n')}</pre>
+    <pre>${result.join('\n')}</pre>
   </div>`;
 }
 
