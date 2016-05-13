26ab1e2ff4f48d87f7326021f61d3cb2ed0c3dd4
Tomusdrw <tomusdrw@gmail.com>
2016-05-13 11:55:30 +0200

    Fixing comments patterns for stylus

--- slides/app/components/read/EditorState.js
+++ slides/app/components/read/EditorState.js
@@ -86,7 +86,7 @@ function parseAnnotations (content, ext, fileName) {
     'go': C_LIKE_PATTERNS,
     'py': HASH_PATTERN,
     'sh': HASH_PATTERN,
-    'styl': HASH_PATTERN,
+    'styl': C_LIKE_PATTERNS,
     'html': HTML_LIKE
   };
   const PATTERN = LINE_PATTERNS[ext];
